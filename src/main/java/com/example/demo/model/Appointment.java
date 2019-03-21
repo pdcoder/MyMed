f900f34f705398d35e4924677e292ffae34117a2
pdcoder <pdas@inversion.co.in>
2019-03-21 19:22:38 +0530

    Added appointment

--- src/main/java/com/example/demo/model/Appointment.java
+++ src/main/java/com/example/demo/model/Appointment.java
@@ -1,58 +1,31 @@
 package com.example.demo.model;
 
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@Getter(AccessLevel.PUBLIC)
+@Setter(AccessLevel.PUBLIC)
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Appointment {
 
     @Id
+    @GeneratedValue
     private Long id;
 
     @ManyToMany
     @JoinColumn(name = "doctor_id",nullable = false)
     private Set<Doctor> doctor ;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "user_id",nullable = false)
     private User user;
 
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
     private String date;
 
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Set<Doctor> getDoctor() {
-        return doctor;
-    }
-
-    public void setDoctor(Set<Doctor> doctor) {
-        this.doctor = doctor;
-    }
-
-
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
 }
